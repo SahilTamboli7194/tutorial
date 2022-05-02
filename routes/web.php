ee2b590de630b3586d72c99b4b8dc3aa878c50be
SahilTamboli7194 <sahiltamboli@instaveritas.com>
2022-05-02 10:39:23 +0530

    End of tutorial three

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Cache;
 use App\Models\Post;
+use App\Models\User;
 use League\CommonMark\Extension\FrontMatter\Data\SymfonyYamlFrontMatterParser;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -19,16 +21,36 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    // \Illuminate\Support\Facades\DB::listen(function ($query){
+    //     logger($query->sql,$query->bindings);
+    // });
     return view('posts',
     [
-        'posts' => Post::All_flies()
+        //'posts' => Post::all()
+        'posts' => Post::latest()->with('category')->get()
     ]);
 });
 
-Route::get('/post/{post}', function ($slug) {
-
-    $post = Post::find($slug);
+Route::get('/post/{post:slug}', function (Post $post) {
+  
+    //$post = Post::findorFail($id);
     
     return view('post',['post' => $post]);
 
 })->where('post','[A-Za-z0-9-]+');
+
+Route::get('category/{category:slug}', function (Category $category){
+    //ddd($category);
+    return view('posts',
+    [
+        'posts' => $category->posts
+    ]);
+});
+
+Route::get('author/{author:username}', function (User $author){
+    //dd($author);
+    return view('posts',
+    [
+        'posts' => $author->posts
+    ]);
+});
